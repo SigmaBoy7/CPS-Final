8725b713ae11a675d83ab72447dc4b4cacdf4b12
Rahmidin Hasanov <rahmiddin.hasanov@bk.ru>
2024-02-20 21:13:25 +0500

    Add tech-block

---
+++ src/blocks/swipers/swipers.js
@@ -0,0 +1,60 @@
+import Swiper from 'swiper';
+import { Pagination } from 'swiper/modules';
+
+const swipersList = document.querySelectorAll('.swiper');
+
+swipersList.forEach(() => {
+
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        slidesPerView: 'auto',
+        modules: [Pagination],
+    
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    
+        breakpoints: {
+            768: {
+                enabled: false
+            }
+        },
+    
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    
+    });
+
+})
+
+
+const showButton = document.querySelectorAll('.swiper__show-button');
+
+showButton.forEach((el, i, arr) => {
+    el.addEventListener('click', () => {
+        const swiperContainer = el.parentElement.querySelector('.swiper__container');
+        const showButtonText = el.querySelector('.swiper__show-button-text');
+        const showButtonIco = el.querySelector('.swiper__show-button-ico');
+        swiperContainer.classList.toggle('swiper__container--big');
+        console.log(el.parentElement)
+        showButtonIco.classList.toggle('rotate')
+        if (showButtonText.textContent === 'Скрыть'){
+            showButtonText.textContent = 'Показать все';
+        }else{
+            showButtonText.textContent = 'Скрыть';
+        };
+        el.classList.toggle('brands__show-button--active')
+    });
+})
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import '../scss/style.scss';
 import burger from '../blocks/burger/burger.js';
 import services from '../blocks/services/services.js';
-import swiper from '../blocks/swiper/swiper.js';
+import swiper from '../blocks/swipers/swipers.js';
 
 console.log('Works!');
